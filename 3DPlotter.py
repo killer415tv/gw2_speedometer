0a047c6a83ac06e793b2bf4e0375d15c61884f9d
kiLLeR415tv <44058571+killer415tv@users.noreply.github.com>
2021-03-04 18:05:35 +0100

    speed meter with time tracks for GWTC guildhall
    and 3D plotter to convert CSV files to 3D graph

---
+++ 3DPlotter.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import plotly.express as px
+import plotly.express as px
+import plotly.graph_objects as go
+import sys, os
+import glob
+import pandas as pd
+
+
+path = os.path.dirname(os.path.abspath(sys.argv[0])) + "/"                  
+all_files = glob.glob(os.path.join(path, "*.csv")) 
+names = [os.path.basename(x) for x in glob.glob(path+'\*.csv')] 
+
+#print(names)
+
+df = pd.DataFrame()
+for file_ in all_files:
+    file_df = pd.read_csv(file_, nrows=6500)
+    file_df['file_name'] = file_
+    df = df.append(file_df)
+
+fig = px.line_3d(df, x="X", y="Z", z="Y", color="file_name")
+#fig = px.scatter_3d(df, x="X", y="Z", z="Y", color="SPEED")
+
+fig.show()
+

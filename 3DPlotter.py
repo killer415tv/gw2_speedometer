0e92a2ac9fa98072b40efccd129b2810342029c6
kiLLeR415tv <44058571+killer415tv@users.noreply.github.com>
2021-03-11 23:32:40 +0100

    Add files via upload

--- 3DPlotter.py
+++ 3DPlotter.py
@@ -19,10 +19,15 @@ for file_ in all_files:
     file_df['file_name'] = file_
     df = df.append(file_df)
 
-# use this for 1 line for each csv
-fig = px.line_3d(df, x="X", y="Z", z="Y", color="file_name")
-# and this for show speed points
-#fig = px.scatter_3d(df, x="X", y="Z", z="Y", color="SPEED")
+    # use this for 1 line for each csv
+#fig = px.line_3d(df, x="X", y="Z", z="Y", color="file_name")
+    # 2d speed time
+#fig = px.line(df, x="ANGLE_BEETLE", y="SPEED", title='SPEED over Time', color="file_name")
+    # and this for show speed points
+#fig = px.scatter_3d(df, x="ACCELERATION", y="SPEED", z="ANGLE_BEETLE", color="SPEED")
+    # 2d speed time
+fig = px.scatter(df, x="ANGLE_BEETLE", y="ACCELERATION", color="SPEED")
+
 
 fig.show()
 
